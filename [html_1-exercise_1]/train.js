4c115ae02fc168b7a930fa7fab799dcae7d77ee2
alialnajjar0 <mkv.ali8x3b@gmail.com>
2023-11-05 22:40:36 +0300

    update changing  BE 1

--- [html_1-exercise_1]/train.js
+++ [html_1-exercise_1]/train.js
@@ -5,7 +5,7 @@ const fs = require("fs")
 
 const server = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'})
-    fs.readFile("[html_1-exercise_1]/main.html", function(error, data){
+    fs.readFile("[html_1-exercise_1]/serverTrain.html", function(error, data){
         if(error){
             res.writeHead(404)
             res.write("Error: File not Found")
